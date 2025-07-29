5ba1048ec4c1f753a6ca60eb07f2217863fed512
Guilherme Monni <gmonni20@gmail.com>
2025-07-28 21:19:04 -0300

    to-do-list dashnote

---
+++ index.js
@@ -0,0 +1,69 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+})
+
+let loginWindow
+let cadastroWindow
+let homeWindow
+
+const createLoginWindow = () => {
+    //criar a janela de login
+    loginWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        resizable: false,
+        webPreferences: {
+            preload: path.join(__dirname, './preload.js')
+        }
+    })
+
+   // loginWindow.setMenu(null)
+    loginWindow.loadFile('index.html')
+}
+
+const createCadastroWindow = () => {
+    //criar janela de cadastro
+    cadastroWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        resizable: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    cadastroWindow.setMenu(null)
+    cadastroWindow.loadFile('cadastro.html')
+}
+
+const createHomeWindow = () => {
+    //criar janela de cadastro
+    homeWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        resizable: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    homeWindow.setMenu(null)
+    homeWindow.loadFile('home.html') 
+}
+
+app.whenReady().then(() => {
+    createLoginWindow()
+
+    ipcMain.on('cadastro', () => {
+        loginWindow.close()
+        createCadastroWindow()
+    })
+
+    ipcMain.on('login-sucesso', () => {
+        loginWindow.close()
+        createHomeWindow()
+    }) 
+})
---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('#formContainer') //formulario
+
+//eventos para verificar login
+form.addEventListener('submit', execLogin)
+form.addEventListener('keyup', (e) => {
+    if(e.type === "Enter"){
+        execLogin()
+    }
+})
+
+async function execLogin(e){ //função para executar login
+    e.preventDefault()
+
+    console.log("verificando os dados inseridos...")
+
+    const dados = new FormData(form) //dados do formulario
+    const username = dados.get('username')
+    const password = dados.get('password')
+
+    try{ //api de conexao
+        const response = await fetch('https://dashnote.onrender.com/login', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ //infos json para login
+                username,
+                password
+            })
+        })
+
+        const result = await response.json()
+        console.log('Resposta servidor:', result)
+        
+        window.location.href = './home.html'
+    }catch(error){
+        console.error('Erro ao fazer o login!', error)
+        alert("Usuário ou senha incorreta!")
+        setTimeout(() => {
+            location.reload()
+        }, 2000)
+    } 
+}
