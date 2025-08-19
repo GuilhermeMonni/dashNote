cd72b0a556ca0c1324b91d1f789cfe415f98e0c1
Guilherme Monni <gmonni20@gmail.com>
2025-08-18 22:11:04 -0300

    create tasks

--- scripts/scriptHome.js
+++ scripts/scriptHome.js
@@ -2,19 +2,18 @@ const token = localStorage.getItem('token')
 
 if(!token){window.location.href = './login.html'}
 
-fetch('https://dashnote.onrender.com/me', { 
+fetch('https://dashnote.onrender.com/me', { //ajustar essa api para retornar o user e o id 
     headers:{
         Authorization: `Bearer ${token}`
     }
 })
-.then(res => { //validacao user 
-    console.log(res)
-
+.then(res => { //validacao user
     if(!res.ok){
         window.location.href = './login.html'
     }
 })
 
-//buscar tasks do usuario
-console.log(user.id)
+async function searchTaks(){
+    
+}
 
