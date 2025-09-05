19c1eefae90f0ca4601d48a645447303fb9e81f5
Guilherme Monni <gmonni20@gmail.com>
2025-09-05 19:07:05 -0300

    add logout user

---
+++ scripts/scriptProfile.js
@@ -0,0 +1,25 @@
+//card profile home 
+
+//exibir o nome do usuario no card 
+let nameCard = document.querySelector('#username-card') 
+let usernameCard = username
+
+usernameCard = usernameCard.charAt(0).toUpperCase() + usernameCard.slice(1)
+
+nameCard.textContent = usernameCard
+
+//logout do usuario
+async function logout(){
+    try{
+        const response = await fetch('https://dashnote.onrender.com/logout', {
+            method:'POST', 
+            credentials: 'include'
+        })
+    }
+    catch{
+        console.error("Falha na requisição de logout:", error)
+    }
+    finally{
+        return window.location.href = './index.html'
+    }
+}
